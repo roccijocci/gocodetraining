c7c202a294190f81b55108c25e0ed3f5e9a0b8ee
b3n3b0 <jags4real25@gmail.com>
2017-09-27 17:55:32 +0100

    all the exercise on sort

--- 21_interfaces/02_package-sort/exercises/main.go
+++ 21_interfaces/02_package-sort/exercises/main.go
@@ -9,6 +9,8 @@ type people []string
 
 //type people []string
 // studyGroup := people{"Zeno", "John", "Al", "Jenny"}
+//s := []string{"Zeno", "John", "Al", "Jenny"}
+// n := []int{7, 4, 8, 2, 9, 19, 12, 32,3}
 
 func (p people) Len() int {
 	return len(p)
@@ -20,8 +22,17 @@ func (p people) Less(i, j int) bool {
 	return p[i] < p[j]
 }
 func main() {
-	studyGroup := people{"Zeno", "yvonne", "Gabriel", "john", "Albert", "Jenny"}
+	//noticed it treats Uppercase and lowercase differently
+	studyGroup := people{"Zeno", "Yvonne", "Gabriel", "John", "Albert", "Jenny"}
+	s := []string{"Zeno", "John", "Al", "Jenny"}
+	n := []int{7, 4, 8, 2, 9, 19, 12, 32, 3}
+	fmt.Println(n)
+	fmt.Println(s)
 	fmt.Println(studyGroup)
+	sort.Ints(n)
+	sort.Strings(s)
 	sort.Sort(studyGroup)
 	fmt.Println(studyGroup)
+	fmt.Println("The sorted Names are", s)
+	fmt.Println("The sorted integers are", n)
 }
