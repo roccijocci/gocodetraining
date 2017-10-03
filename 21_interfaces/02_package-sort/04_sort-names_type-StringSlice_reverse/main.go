755c63329d74c26734bd9aa00caa48590d62d703
b3n3b0 <jags4real25@gmail.com>
2017-10-03 20:32:33 +0100

    just another commit

--- 21_interfaces/02_package-sort/04_sort-names_type-StringSlice_reverse/main.go
+++ 21_interfaces/02_package-sort/04_sort-names_type-StringSlice_reverse/main.go
@@ -24,14 +24,14 @@ func main() {
 	t := sort.StringSlice(s)
 	fmt.Printf("just t: %T\n", t)
 
-	fmt.Printf("s converted to StringSlice: %T\n", sort.StringSlice(s))
+	// fmt.Printf("s converted to StringSlice: %T\n", sort.StringSlice(s))
 	// fmt.Printf("s sorted: %T\n", sort.Sort(sort.StringSlice(s)))
-	fmt.Printf("s reversed: %T\n", sort.Reverse(sort.StringSlice(s)))
-	i := sort.Reverse(sort.StringSlice(s))
-	fmt.Println(i)
-	fmt.Printf("%T\n", i)
-	sort.Sort(i)
-	fmt.Println(s)
+	// fmt.Printf("s reversed: %T\n", sort.Reverse(sort.StringSlice(s)))
+	// i := sort.Reverse(sort.StringSlice(s))
+	// fmt.Println(i)
+	// fmt.Printf("%T\n", i)
+	// sort.Sort(i)
+	// fmt.Println(s)
 }
 
 // https://golang.org/pkg/sort/#Sort
